80adaeb89f1ed0d7b03cb918a610528a8cbccbfc
priscilla-e <56302477+priscilla-e@users.noreply.github.com>
2025-09-03 23:04:24 +0100

    Update vite-config compactibility with cloudflare

--- minglib_ai/client/vite.config.ts
+++ minglib_ai/client/vite.config.ts
@@ -5,4 +5,14 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: true,           // listen on all interfaces (important for tunneling)
+    strictPort: true,     // don’t auto-switch ports
+    allowedHosts: [
+      '.trycloudflare.com'   // allow any *.trycloudflare.com host
+    ],
+    hmr: {
+      clientPort: 443     // needed for HMR when using HTTPS tunnel
+    }
+  }
 })
